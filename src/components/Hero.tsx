41654fb93e23175ae2542cd73f6148a04edbdb59
codigoinerte <fmartinez.bpe@gmail.com>
2026-04-16 21:57:44 -0500

    fix: cambio de componentes en homeScreen

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,24 @@
+import { Social } from "./Social"
+
 export const Hero = () => {
     return (
-        <div>Hero</div>
+        <>
+            <div className='hero'></div>
+            {/* Hero section */}
+            <div className="container-fluid home">
+                <div>
+                <h2>Welcome to my page</h2>
+                <h1>
+                    Hi,<br />
+                    I'm Fredy,<br />
+                    <span className="typewrite" data-period="2000"></span>
+                    <span className="wrap"></span>
+                </h1>
+
+                <a href="mailto:fmartinez.bpe@gmail.com">Contact me!</a>
+                </div>
+                <Social />
+            </div> 
+        </>
     )
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect } from 'react'
 import '../assets/styles/home.scss';
-import { Header, Social } from '../components';
+import { Header } from '../components';
+import { Hero } from '../components/Hero';
 
 interface TxtTypeInstance {
   toRotate: string[];
@@ -93,23 +94,11 @@ export const HomeScreen: FC = () => {
 
   return (
     <>
+      {/* header */}
       <Header />
-      <div className='hero'></div>
-      {/* Hero section */}
-      <div className="container-fluid home">
-        <div>
-          <h2>Welcome to my page</h2>
-          <h1>
-            Hi,<br />
-            I'm Fredy,<br />
-            <span className="typewrite" data-period="2000"></span>
-            <span className="wrap"></span>
-          </h1>
 
-          <a href="mailto:fmartinez.bpe@gmail.com">Contact me!</a>
-        </div>
-        <Social />
-      </div>
+      {/* hero */}
+      <Hero />
     </>
   )
 }
