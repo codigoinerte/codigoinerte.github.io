3c3c275b9fc36959802f0cfdeab4b5aecbf52a02
codigoinerte <fmartinez.bpe@gmail.com>
2026-05-01 17:16:21 -0500

    fix: cambio de next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/codigoinerte.github.io',
+  assetPrefix: '/codigoinerte.github.io/',
   trailingSlash: true,
   images: {
     unoptimized: true,
